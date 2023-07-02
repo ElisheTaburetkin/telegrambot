8065f05c66fc036279e523125040fb2488701b59
elishe <elisei2508@gmail.com>
2023-07-02 16:12:17 +0300

    upd

--- bot_config.py
+++ bot_config.py
@@ -1,2 +1,2 @@
-TOKEN = '6122856273:AAEpGwiLXPBfaZ9HSlzMuD9kwpWfmjJKJek' # Enter your TG bot token here
-ADMINUID = '' # Enter your TG id here
+TOKEN = '5830853476:AAFpcN5qHFKseGTCiMAsba1ki9gR3n_op4w' # Enter your TG bot token here
+ADMINUID = ['',''] # Enter your TG id here
