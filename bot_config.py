a233007566f04e0b89c9be48da724c7aa17fb3bc
Egor <129303990+egobzh@users.noreply.github.com>
2023-07-05 13:38:17 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,2 +1,2 @@
-TOKEN = '5660719515:AAG60l687oK--b6-DKGhGV9H11nQrxw1AQ4' # Enter your TG bot token here
-ADMINUID = ['726079589'] # Enter your TG id here
+TOKEN = '' # Enter your TG bot token here
+ADMINUID = ['',''] # Enter your TG id here
